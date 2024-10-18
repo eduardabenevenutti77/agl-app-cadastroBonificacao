dec76dbb623e97c2b284fe64c82c2702c04d6a99
Maria <eduardabenevenutti77.com.br>
2024-10-18 19:00:54 -0300

    fix: app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Cadastro from './pages/Cadastro';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Footer from './components/Footer';
-import Cadastrogestor from './pages/Cadastro-gestor';
 import Login from './pages/Login';
 import DashboardGestor from './pages/Dashboard-gestor';
 import { AuthProvider } from '../src/auth/Context'
@@ -26,13 +24,13 @@ function App() {
         {!ocultarElementos && <Header />}
         <div className="content">
           <Routes>
-          <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Cadastro />} />
-            <Route element={<PrivateRoute />}>
-            <PrivateRoute path="/cadastrogestor" component={Cadastrocomissao} />
-              <Route path="/dashboardgestor" element={<DashboardGestor />} />
-              <Route path="/sobre" element={<Sobre />} />
-            </Route>
+            <Route path="/login" element={<Login />} />
+              <Route path="/" element={<Cadastro />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="/cadastrogestor" component={Cadastrocomissao} />
+                <Route path="/dashboardgestor" element={<DashboardGestor />} />
+                <Route path="/sobre" element={<Sobre />} />
+              </Route>
           </Routes>
         </div>
         <ToastContainer
